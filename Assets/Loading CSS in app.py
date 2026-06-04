4edabac512ef5a18e32f256db8ff224d941198de
manjunath9887 <manjunath.cse9@gmail.com>
2026-06-04 12:10:38 +0530

    Create Loading CSS in app.py

---
+++ Assets/Loading CSS in app.py
@@ -0,0 +1,9 @@
+def load_css():
+
+    with open("assets/style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
